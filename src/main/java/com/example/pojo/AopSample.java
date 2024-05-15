2b291e46a5d086605209d0a01da58b9f0e7bfac4
gahyeonkwon <zkvmflcldh0@gmail.com>
2024-05-15 23:37:17 +0900

    aop sample code 작성 2

--- src/main/java/com/example/pojo/AopSample.java
+++ src/main/java/com/example/pojo/AopSample.java
@@ -1,26 +1,26 @@
 package com.example.pojo;
 
 import com.example.pojo.LocalExcutionTime;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class AopSample {
 
     @LocalExcutionTime
     @GetMapping("/test1")
     public String test1 () {
 
-        System.out.println("service code");
+        log.info("비즈니스로직이 호출됩니다.");
 
         return "test1";
     }
 
-    @LocalExcutionTime
+    @TestAOP
     @GetMapping("/test2")
-    public String test2 () {
-        System.out.println(" service code2");
-
-        return "test2";
+    public void test2 ()  {
+        throw new NullPointerException("hello world!");
     }
 }
--- src/main/java/com/example/pojo/LogAspect.java
+++ src/main/java/com/example/pojo/LogAspect.java
@@ -2,9 +2,10 @@ package com.example.pojo;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
@@ -12,18 +13,44 @@ import org.springframework.util.StopWatch;
 @Component // @Configuration 으로 사용 권장
 @Aspect // aspectj 에서 제공, 횡단 관심사 클래스임을 표기함
 public class LogAspect {
-    @Around("@annotation(LocalExcutionTime)") // Advice의 한 종류로 Aspect의 실패 여부와 상관없이 전,후로 실행되도록하는 Advice. 파라미터로 pointCut 전달 필요
+
+    @Pointcut("@annotation(LocalExcutionTime)")
+    public void pointcut() {}
+
+    //@Around("@annotation(LocalExcutionTime)") // Advice의 한 종류로 Aspect의 실패 여부와 상관없이 전,후로 실행되도록하는 Advice. 파라미터로 pointCut 전달 필요
+    @Around("pointcut()")
     // pointCut은 Aspect가 적용될 joinpoint를 정의 함
 
     public Object LocalExcutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        Object proceed = proceedingJoinPoint.proceed();
+        log.info("proceed 호출 전 ");
+
+        //Object proceed = proceedingJoinPoint.proceed();
 
         stopWatch.stop();
         log.info(stopWatch.prettyPrint());
+        log.info("proceed 호출 후 ");
 
-        return proceed;
+        return null;
+        //return proceed;
     }
+
+
+    @Pointcut("@annotation(TestAOP)")
+    public void TestAOP() {}
+
+    @AfterThrowing("TestAOP()")
+    public void afterThrowingTest(JoinPoint joinPoint)  {
+        Signature signature = joinPoint.getSignature();
+        log.info("throwing .. " + signature.getName());
+    }
+
+    @AfterReturning("TestAOP()")
+    public void afterReturningTest(JoinPoint joinPoint)  {
+        Signature signature = joinPoint.getSignature();
+        log.info("returning .. " + signature.getName());
+    }
+
 }
